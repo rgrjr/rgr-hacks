b9c4810ed7156a1dd6264e04fd6049b9928bb557
rgr <rgr@e03adc71-3023-0410-be83-de118f9f4c1e>
2004-08-06 03:05:57 +0000

    New rgr-cvs-insert-log-skeleton and rgr-cvs-plus hacks.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@161 e03adc71-3023-0410-be83-de118f9f4c1e

--- rgr-cvs-hacks.el
+++ rgr-cvs-hacks.el
@@ -29,4 +29,53 @@ that many days."
     (shell-command (format "cvs -q log -d '>%s' | cvs-chrono-log.pl"
 			   n-days-ago))))
 
+;;;###autoload
+(defun rgr-cvs-insert-log-skeleton ()
+  "Insert a '* filename:' line for each 'Index: filename' line in shell output.
+This goes through the '*Shell Command Output*' buffer, which is assumed
+to be the output of 'cvs diff', inserting lines at the end of the
+current buffer, which is assumed to be something like a CVS log comment.
+Files that are being added or deleted are noted as such."
+  (interactive)
+  (let ((original-buffer (current-buffer))
+	(other-buffer
+	  (or (get-buffer "*Shell Command Output*")
+	      (error "Can't find '*Shell Command Output*' buffer.")))
+	(match-re (concat "^Index: \\([^ \t\n]*\\)$"
+			  "\\|^cvs diff: \\([^ \t\n]*\\) "
+			  "\\(is a new entry\\|was removed\\)")))
+    (save-excursion
+      (set-buffer other-buffer)
+      (goto-char (point-min))
+      (while (re-search-forward match-re nil t)
+	(let ((file-name (or (match-string 1) (match-string 2)))
+	      (extra (match-string 3)))
+	  (message "found %S" file-name)
+	  (save-excursion
+	    (set-buffer original-buffer)
+	    (goto-char (point-max))
+	    (insert "* " file-name
+		    (cond ((equal extra "was removed") " (deleted)")
+			  (extra " (added)")
+			  (t ""))
+		    ":\n")))))
+    ;; move point to start adding comments.
+    (or (eobp)
+	(forward-line 1))))
+
+(defun rgr-cvs-plus ()
+  "Insert a '  + ' at the beginning of the current line."
+  (interactive)
+  (cond ((not (bolp))
+	  (save-excursion
+	    (beginning-of-line)
+	    (rgr-cvs-plus)))
+	(t
+	  (cond ((looking-at "^ *\\* ")
+		  (insert "\n")
+		  (forward-line -1)))
+	  (insert "   + "))))
+
+(define-key text-mode-map "\C-c+" 'rgr-cvs-plus)
+
 (provide 'rgr-cvs-hacks)
