e3a8bad0c637eeac74bc09c94a9087945c2d889d
rogers <rogers@e03adc71-3023-0410-be83-de118f9f4c1e>
2004-10-30 02:08:22 +0000

    + (rgr-renumber-region-paragraphs):  Handle multiple components      separated by dots, e.g. 3.5.7 comes after 3.5.6.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@173 e03adc71-3023-0410-be83-de118f9f4c1e

--- rgr-enumerate.el
+++ rgr-enumerate.el
@@ -4,8 +4,52 @@
 ;;;
 ;;; $Id$
 
+(defun rgr-renumber-explode-dots (string)
+  (let ((result nil) (start 0))
+    (save-match-data
+      (while (string-match "\\([0-9]+\\)\\.?" string start)
+	(setq result (cons (string-to-int (match-string 1 string)) result))
+	(setq start (match-end 0))))
+    (nreverse result)))
+
+;; (rgr-renumber-explode-dots "3.5.7")
+
 ;;;###autoload
 (defun rgr-renumber-region-paragraphs (start end)
+  "If the region contains a series of numbered paragraphs, renumber them
+sequentially, beginning with the first.  Numbers must have a trailing
+dot, and may consist of two or more dot-separated subcomponents, which
+are incremented lexicographically."
+  (interactive "r")
+  (let ((last nil))
+    (save-excursion
+      (goto-char start)
+      (while (re-search-forward "^[ \t]+\\(\\([0-9.]+\\.\\)?[0-9]+\\)\\. "
+				end t)
+	(let ((dots (rgr-renumber-explode-dots (match-string 1))))
+	  (cond ((not dots))
+		((not last)
+		  (setq last dots))
+		(t
+		  (let ((ll (length last)) (ld (length dots)))
+		    (cond ((> ld ll)
+			    ;; down to a lower level; take the initial value of
+			    ;; the new dotted component(s) as given.
+			    (setcdr (nthcdr (1- ll) last)
+				    (nthcdr (1- ll) dots)))
+			  (t
+			    (if (> ll ld)
+				;; up to a higher level
+				(setcdr (nthcdr (1- ld) last) nil))
+			    (setcar (nthcdr (1- ld) last)
+				    (1+ (car (nthcdr (1- ld) last))))))
+		    (replace-match (mapconcat (function (lambda (x)
+						(format "%d" x)))
+					      last ".")
+				   t t nil 1)))))))))
+
+;; this just handles single-component numbers.
+(defun rgr-old-renumber-region-paragraphs (start end)
   "If the region contains a series of numbered paragraphs, renumber them
 sequentially, beginning with the first."
   (interactive "r")
