e606b39238ed223d68ca30dbd139f791189aed34
rgr <rgr@e03adc71-3023-0410-be83-de118f9f4c1e>
2006-04-10 15:46:10 +0000

    + More gratuitous dreck.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@281 e03adc71-3023-0410-be83-de118f9f4c1e

--- foo-dired.el
+++ foo-dired.el
@@ -3,20 +3,6 @@
 ;;;    Useful stuff in here is rgr-dired-rename-file-and-versions and
 ;;; rgr-dired-sort-by-size (but the latter is buggy).
 ;;;
-;;;    To compile this (almost) without error, do the following:
-;;;
-;;;	(mapcar 'require '(dired dired-aux))
-;;;
-;;;    Modification history:
-;;;
-;;; rgr-dired-sort-by-size: new, but semi-kludgey, fix
-;;;	rgr-exchange-point-and-mark C-u bug.  -- rgr, 21-Feb-98.
-;;; rgr-dired-sort-by-size: made sticky.  -- rgr, 26-Feb-98.
-;;; rgr-dired-rename-file-and-versions: new hack.  -- rgr, 2-Feb-99.
-;;; split out of the rgr-hacks.el file.  -- rgr, 10-Sep-99.
-;;; rgr-dired-rename-file-and-versions: require dired-aux.  -- rgr, 16-Jan-01.
-;;; rgr-dired-do-size-sort: fix sort arg for Linux version.  -- rgr, 4-Aug-01.
-;;;
 
 ;; [another gratuitous change.  -- rgr, 7-Apr-06.]
 
