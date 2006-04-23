94b737d13bb6ce05b8efe1c0f1c3d5a700878c19
rogers <rogers@e03adc71-3023-0410-be83-de118f9f4c1e>
2006-04-23 17:39:53 +0000

    + (ffap-url-encode-hex):  Made case-insensitive, more robust.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@289 e03adc71-3023-0410-be83-de118f9f4c1e

--- ffap-local-url-patch.el
+++ ffap-local-url-patch.el
@@ -7,7 +7,10 @@
 (require 'ffap)
 
 (defun ffap-url-encode-hex (string)
-  ;; Convert a hex string to an integer.  Breaks if given non-hex chars.
+  ;; Convert a hex string to an integer.  Signals an error if given non-hex
+  ;; chars (including whitespace!).  [This is like hex-char-to-num from
+  ;; gnus/hex-util.el, which is defined only while compiling that file.  -- rgr,
+  ;; 23-Apr-06.]
   (let ((hex-digit-alist '((?0 .  0)  (?1 .  1)  (?2 .  2)  (?3 .  3)
 			   (?4 .  4)  (?5 .  5)  (?6 .  6)  (?7 .  7)
 			   (?8 .  8)  (?9 .  9)  (?A . 10)  (?B . 11)
@@ -15,7 +18,9 @@
 	(result 0) (i 0))
     (while (< i (length string))
       (setq result (+ (* result 16)
-		      (cdr (assq (aref string i) hex-digit-alist))))
+		      (or (cdr (assq (upcase (aref string i)) hex-digit-alist))
+			  (error "Non-hex digit at position %d in %S."
+				 i string))))
       (setq i (1+ i)))
     result))
 
@@ -33,7 +38,7 @@
 		     t t string)))
     string))
 
-;; (equal (ffap-url-decode-string "foo%20bar+baz%25EFquux%23") "foo bar baz%EFquux#")
+;; (equal (ffap-url-decode-string "foo%20bar+%25EFquux%23") "foo bar %EFquux#")
 
 ;; [was originally a defsubst.  patched to wrap ffap-url-decode-string around
 ;; the result.  -- rgr, 9-Aug-03.]
