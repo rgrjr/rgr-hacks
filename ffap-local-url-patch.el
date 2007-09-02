1fa0afb894b98baf88802cbfd210abe23fa4712e
rogers <rogers@e03adc71-3023-0410-be83-de118f9f4c1e>
2007-09-02 23:01:52 +0000

    + (ffap-url-decode-string):  Use decode-hex-string (in hex-util)      instead of our own ffap-url-encode-hex.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@370 e03adc71-3023-0410-be83-de118f9f4c1e

--- ffap-local-url-patch.el
+++ ffap-local-url-patch.el
@@ -6,35 +6,16 @@
 
 (require 'ffap)
 
-(defun ffap-url-encode-hex (string)
-  ;; Convert a hex string to an integer.  Signals an error if given non-hex
-  ;; chars (including whitespace!).  [This is like hex-char-to-num from
-  ;; gnus/hex-util.el, which is defined only while compiling that file.  -- rgr,
-  ;; 23-Apr-06.]
-  (let ((hex-digit-alist '((?0 .  0)  (?1 .  1)  (?2 .  2)  (?3 .  3)
-			   (?4 .  4)  (?5 .  5)  (?6 .  6)  (?7 .  7)
-			   (?8 .  8)  (?9 .  9)  (?A . 10)  (?B . 11)
-			   (?C . 12)  (?D . 13)  (?E . 14)  (?F . 15)))
-	(result 0) (i 0))
-    (while (< i (length string))
-      (setq result (+ (* result 16)
-		      (or (cdr (assq (upcase (aref string i)) hex-digit-alist))
-			  (error "Non-hex digit at position %d in %S."
-				 i string))))
-      (setq i (1+ i)))
-    result))
-
 (defun ffap-url-decode-string (string)
-  ;; [based on the vm-url-decode-buffer fn, which is GPL'ed with the following
-  ;; notice: "Copyright (C) 1989-1997 Kyle E. Jones".  -- rgr, 9-Aug-03.]
   (let ((pos 0))
     (while (string-match "%[0-9a-fA-F][0-9a-fA-F]\\|\\+" string pos)
+      (require 'hex-util)	;; for decode-hex-string
       (setq pos (1+ (match-beginning 0)))
       (setq string (replace-match
 		     (if (= pos (match-end 0))
 			 " "
-			 (string (ffap-url-encode-hex
-				   (substring string pos (match-end 0)))))
+			 (decode-hex-string
+			   (substring string pos (match-end 0))))
 		     t t string)))
     string))
 
