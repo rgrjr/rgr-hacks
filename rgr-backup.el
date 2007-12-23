988a1d10eb988fbc6be58f7d4d2862d9dbc5638b
rogers <rogers@e03adc71-3023-0410-be83-de118f9f4c1e>
2007-12-23 00:11:25 +0000

    + (rgr-add-backup-log-entry):  Generalize the search loop regexp to      include LVM partition names, such as "/dev/mapper/alt-home".    + (rgr-backup-change-stars-to-dots):  Use string-to-number.    + (rgr-add-backup-log-entry):  Ditto.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@387 e03adc71-3023-0410-be83-de118f9f4c1e

--- rgr-backup.el
+++ rgr-backup.el
@@ -13,7 +13,7 @@
   (let ((other-level nil))
     (beginning-of-line)
     (while (and (looking-at "^[ \t]*\\([^ \t\n]\\)[ \t]*\\([0-9]\\) ")
-		(setq other-level (string-to-int (match-string 2)))
+		(setq other-level (string-to-number (match-string 2)))
 		(<= level other-level))
       ;; [normally, the flag char is one of ".*x".  -- rgr, 1-Jun-05.]
       ;; (message "[other-level %s, flag char %S]" other-level (match-string 1))
@@ -25,7 +25,7 @@
 (defun rgr-add-backup-log-entry ()
   (interactive)
   (goto-char (point-min))
-  (while (re-search-forward "^Backing up \\(/dev/[a-z0-9]+ ([^ ()]+)\\)" nil t)
+  (while (re-search-forward "^Backing up \\(/dev/[-a-z/0-9]+ ([^ ()]+)\\)" nil t)
     (let ((partition (match-string 1))
 	  level date size)
       (forward-line 1)
@@ -33,7 +33,7 @@
 	   ;; creating a new /etc/dumpdates file.
 	   (forward-line 1))
       (and (looking-at " *DUMP: Date of this level \\([0-9]\\) dump: \\(.*\\)")
-	   (setq level (string-to-int (match-string 1))
+	   (setq level (string-to-number (match-string 1))
 		 date (match-string 2)))
       (if (save-excursion
 	    (re-search-forward "blocks (\\([0-9.]+\\)MB) on " nil t))
