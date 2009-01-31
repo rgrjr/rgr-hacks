b57a6d97800334c9406953c7f4d7bb466c1a3299
rogers <rogers@e03adc71-3023-0410-be83-de118f9f4c1e>
2009-01-31 23:07:50 +0000

    * rgr-compile-hacks.el:    + (rgr-grep-annotate-current-buffer):  Get rid of this, which was      never very useful in the first place, and has since bit-rotted due      to grep-buffer internals changes.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@475 e03adc71-3023-0410-be83-de118f9f4c1e

--- rgr-compile-hacks.el
+++ rgr-compile-hacks.el
@@ -49,113 +49,6 @@
 			   rgr-grep-summarize-hits-command-string))
     (set-window-start (display-buffer buffer) 1)))
 
-;;;; Annotating a grep buffer with Lisp definition names.
-
-;; A more general mechanism (e.g. one that knew about perl or C definitions) is
-;; devoutly to be desired.
-
-(defun rgr-grep-fixup-next-error (next-error)
-  ;; Fixup cons of (error-marker . source-spec) by side-effect.  This is done by
-  ;; converting source-spec into a marker if it is not one already (or nil),
-  ;; returning next-error.  [Based on some wild code, slightly tamed, in a while
-  ;; test in the compilation-next-error-locus fn.  -- rgr, 27-Feb-02.]
-  (cond ((or (null (cdr next-error))
-	     (markerp (cdr next-error)))
-	  ;; This error has already been done (or is boring).
-	  next-error)
-	(t
-	  ;; This error has a filename/lineno pair.
-	  ;; Find the file and turn it into a marker.
-	  (let* ((fileinfo (car (cdr next-error)))
-		 (buffer (apply 'compilation-find-file
-				(car next-error) fileinfo)))
-	    (if (null buffer)
-		;; We can't find this error's file.
-		(setcdr next-error nil)
-		;; We found the file.  Get a marker for this error.
-		;; compilation-error-screen-columns is buffer-local so we must
-		;; be careful to extract its value before switching to the
-		;; source file buffer.
-		(let ((columns compilation-error-screen-columns)
-		      (error-line (nth 1 (cdr next-error)))
-		      (error-column (nth 2 (cdr next-error))))
-		  (set-buffer buffer)
-		  (save-excursion
-		    (save-restriction
-		      (widen)
-		      (goto-line error-line)
-		      ;; Columns in error msgs are 1-origin.
-		      (cond ((or (not error-column) (zerop error-column))
-			      (beginning-of-line))
-			    (columns
-			      (move-to-column (1- error-column)))
-			    (t
-			      (forward-char (1- error-column))))
-		      (setcdr next-error (point-marker))))))
-	    next-error))))
-
-;;;###autoload
-(defun rgr-grep-annotate-current-buffer (current-buffer &optional msg)
-  ;; intended for compilation-finish-functions list usage.
-  ;; [for the rgr-lisp-def-name fn.  -- rgr, 27-Feb-02.]
-  (require 'rgr-lisp-hacks)
-  (let ((tail compilation-error-list) (defn-name nil)
-	(last-definition-buffer nil)
-	(last-definition-point nil))
-    (while tail
-      (let* ((next-error (rgr-grep-fixup-next-error (car tail)))
-	     (error-marker (car next-error))
-	     (source-marker (cdr next-error)))
-	(and source-marker
-	     (set-buffer (marker-buffer source-marker)))
-	(cond ((null source-marker))
-	      ((not (member major-mode '(emacs-lisp-mode fi:common-lisp-mode
-					 common-lisp-mode lisp-mode)))
-		;; bug: only know how to do Lisp modes.
-		)
-	      ((progn (goto-char source-marker)
-		      (if (and (not (bobp))
-			       (not (looking-at "^(")))
-			  (beginning-of-defun))
-		      (not (looking-at "^(")))
-		;; couldn't find a defun.
-		)
-	      ((and (eq (current-buffer) last-definition-buffer)
-		    (eq (point) last-definition-point))
-		;; still in the same definition
-		)
-	      ((setq defn-name (rgr-lisp-def-name))
-		(setq last-definition-buffer (current-buffer))
-		(setq last-definition-point (point))
-		(set-buffer (marker-buffer error-marker))
-		(goto-char error-marker)
-	        (let ((defn-name-start (point))
-		      (defn-name-end nil))
-		  (insert-before-markers defn-name)
-		  (setq defn-name-end (point))
-		  (goto-char defn-name-start)
-		  ;; skip past the "defun " (or whatever).
-		  (skip-chars-forward "^ \t")
-		  (skip-chars-forward " \t")
-		  (put-text-property (point) defn-name-end
-				     'face 'font-lock-function-name-face)
-		  ;; get back to where we were inserting.
-		  (goto-char defn-name-end))
-	        (insert-before-markers" :\n"))))
-      (setq tail (cdr tail)))))
-
-;;;###autoload
-(defun rgr-grep-annotate-with-definition-names ()
-  "Annotate the output of the last \\[grep] command with Lisp definition
-names for each hit."
-  ;; this requires the ilisp-possibilities stuff.
-  (interactive)
-  (require 'ilisp-possibilities)
-  (save-excursion
-    (set-buffer (or (ilisp-current-possibility-buffer)
-		    (error "No current possibilities buffer.")))
-    (rgr-grep-annotate-current-buffer (current-buffer))))
-
 ;;;; rgr-recompile
 
 (defun rgr-remember-compile-command ()
@@ -231,16 +124,6 @@ without asking any questions."
 
 ;;;###autoload
 (defun rgr-compilation-mode-hook ()
-  ;; [rgr-grep-annotate-current-buffer is getting to be a pain, as it insists on
-  ;; reading all of the files that were hit, and doesn't always work very well
-  ;; to begin with, and rgr-maybe-clean-c-compilation-buffer is moot, as I so
-  ;; rarely code in C any more.  -- rgr, 7-Aug-03.]
-  ;; [rgr-maybe-clean-c-compilation-buffer deleted.  -- rgr, 18-Jan-08.]
-  '(mapcar (function (lambda (fn)
-	    (or (member fn compilation-finish-functions)
-		(setq compilation-finish-functions
-		      (cons fn compilation-finish-functions)))))
-	  '(rgr-grep-annotate-current-buffer))
   ;; Customize the window height.  This is 1/4 of the frame, but never less than
   ;; six lines.  [stolen from the rgr-rmail-mode-hook version.  -- rgr,
   ;; 10-Oct-00.]
