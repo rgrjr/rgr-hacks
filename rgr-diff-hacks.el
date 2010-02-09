f8a4006e5b7497ec26bf6268f6671442398ff0c7
rgr <rgr@e03adc71-3023-0410-be83-de118f9f4c1e>
2010-02-09 16:15:58 +0000

    + (rgr-diff-internal, rgr-quick-source-compare):  Remove these, as we      have been using diff-buffer-with-file for a few years now.
    git-svn-id: https://rgrjr.dyndns.org/svn/rgr-hacks/trunk@516 e03adc71-3023-0410-be83-de118f9f4c1e

--- rgr-diff-hacks.el
+++ rgr-diff-hacks.el
@@ -27,57 +27,6 @@
   (require 'diff)
   (require 'diff-mode))
 
-(defun rgr-diff-internal (old new &optional switches new-file-is-temp-p)
-  ;; based on the discus-diff function, with a few tricks from the diff
-  ;; function.
-  (let* ((old-alt (file-local-copy old))
-	 ;; (compilation-process-setup-function 'diff-process-setup)
-	 ;; Prefer explicitly specified switches.
-	 (args (append (let ((switches (or switches diff-switches)))
-			 (if (listp switches)
-			     switches
-			     (list switches)))
-		       (list (or old-alt old) new))))
-    (save-excursion
-      (set-buffer (get-buffer-create "*diff*"))
-      (toggle-read-only -1)
-      (erase-buffer)
-      (display-buffer (current-buffer) t)
-      (apply (function call-process) diff-command nil (current-buffer) t args)
-      (if old-alt
-	  (delete-file old-alt))
-      (if new-file-is-temp-p
-	  ;; Replace the "new" file name with the old file name in the output.
-	  ;; Since the new file is a temporary file, this is necessary to get
-	  ;; diff-mode commands to operate on the correct file.  [bug:  this
-	  ;; regexp only works for "diff -u".  -- rgr, 24-Aug-05.]
-	  (save-excursion
-	    (goto-char (point-min))
-	    (while (re-search-forward (concat "^\\+\\+\\+ "
-					      (regexp-quote new) "\t")
-				      nil t)
-	      (replace-match (concat "+++ " old "\t") t t))))
-      (diff-mode)
-      (current-buffer))))
-
-;;;###autoload
-(defun rgr-quick-source-compare ()
-  "Run diff on a buffer and the disk version of the file."
-  (interactive)
-  (let* ((file (or (buffer-file-name)
-		   (error "Not a file buffer.")))
-	 (tmp-dir (or temporary-file-directory (getenv "TMPDIR")
-		      (getenv "TMP") "/tmp"))
-	 (tmp (make-temp-name (expand-file-name "rgr-diff-" tmp-dir))))
-    (unwind-protect
-	(progn (save-restriction
-		 (widen)
-		 (write-region (point-min) (point-max) tmp))
-	       (rgr-diff-internal file tmp nil t))
-      (condition-case ignore
-	  (delete-file tmp)
-	(error nil)))))
-
 ;;;###autoload
 (defun rgr-diff-patch-directory (dir)
   "In a patch-mode buffer, apply it to a specified directory."
@@ -127,8 +76,6 @@
 
 ;;;###autoload
 (defun rgr-install-diff-hacks ()
-  ;; [this used to bind rgr-quick-source-compare, but diff-buffer-with-file
-  ;; seems to have been around for a while.  -- rgr, 27-Oct-07.]
   (global-set-key "\C-c=" 'diff-buffer-with-file)
   (global-set-key "\C-c!" 'rgr-add-definition-comment)
   (add-hook 'diff-mode-hook 'rgr-diff-mode-hook))
