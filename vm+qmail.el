3299b26c07b389877339eab1c13543de760e4c06
Bob Rogers <rogers@rgrjr.com>
2021-07-24 14:50:29 -0400

    * vm+qmail.el:    + (v+q-mbox-status-internal):  Drop exceptions for the "spam" and      "forged" inboxes.

--- vm+qmail.el
+++ vm+qmail.el
@@ -119,10 +119,9 @@ inboxes/maildirs (so be careful of . and ..!).")
 	  ;; See if there's any mail in inbox.
 	  (let* ((inbox (car tail))
 		 (n-messages
-		   (cond ((string-match "/\\(spam\\|forged\\)$" inbox) 0)
-		   ((file-directory-p inbox)
-		     (v+q-maildir-incoming-message-count inbox))
-		   (t (v+q-incoming-message-count inbox)))))
+		   (cond ((file-directory-p inbox)
+			   (v+q-maildir-incoming-message-count inbox))
+			 (t (v+q-incoming-message-count inbox)))))
 	    (if (> n-messages 0)
 		(let ((inbox-short-name
 			(if dir-printed-p
